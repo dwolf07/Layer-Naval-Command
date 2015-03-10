94d9f154d30be5289d71134c26443d66239eb3a8
dwolf07 <dwolf07@gmail.com>
2015-03-09 22:27:38 -0700

    Initial setup
    - Started laying the groundwork for the project

---
+++ Assets/Scripts/Gameplay/Global.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Global {
+
+	//Bow of a given ship (coupled with a 
+	public class Coordinate{
+	}
+
+	//Direction of a given ship (used for rendering and hit detection)
+	public enum Heading {East, South, West, North};
+}
---
+++ Assets/Scripts/Gameplay/Ships/Ship.cs
@@ -0,0 +1,24 @@
+﻿using Global;
+using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+
+	private Coordinate location;
+	private Heading direction;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public bool isSunk(){
+		return false;
+	}
+}
